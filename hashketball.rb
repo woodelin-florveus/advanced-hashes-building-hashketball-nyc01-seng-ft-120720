f14fb97ff8ac649b7321a7667afa5421c8e066c2
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 20:59:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,7 +12,7 @@ game_stat = {
     :colors => ["Black", "White"]
     :players => [
             {
-              player_name: "Alan Anderson",
+          player_name: "Alan Anderson",
           number: 0,
           shoe: 16,
           points: 22,
