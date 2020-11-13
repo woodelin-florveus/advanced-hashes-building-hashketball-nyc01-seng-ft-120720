328a8c0c760c7866cba2fecb28bb2b4949106e48
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:06:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,15 +3,12 @@
 
 
 def game_hash
-
-# game_stat = Hash.new
-
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
         {
-  :home => {
-    :team_name => "Brooklyn Nets",
-    :colors => ["Black", "White"]
-    players: [
-            {
           player_name: "Alan Anderson",
           number: 0,
           shoe: 16,
@@ -61,71 +58,65 @@ def game_hash
           steals: 4,
           blocks: 11,
           slam_dunks: 1
-          }
-        ]
-      },
-  :away => {
-    :team_name => "Charlotte Hornets",
-    :colors => ["Turquoise", "Purple"],
-    :players => [
-
-      {
-
-        player_name: "Jeff Adrien",
-        number: 4,
-        shoe: 18,
-        points: 10,
-        rebounds: 1,
-        assists: 1,
-        steals: 2,
-        blocks: 7,
-        slam_dunks: 2
-      }, {
-        player_name: "Bismak Biyombo",
-        number: 0,
-        shoe: 16,
-        points: 12,
-        rebounds: 4,
-        assists: 7,
-        steals: 7,
-        blocks: 15,
-        slam_dunks: 10
-      }, {
-        player_name: "DeSagna Diop",
-        number: 2,
-        shoe: 14,
-        points: 24,
-        rebounds: 12,
-        assists: 12,
-        steals: 4,
-        blocks: 5,
-        slam_dunks: 5
-      }, {
-        player_name: "Ben Gordon",
-        number: 8,
-        shoe: 15,
-        points: 33,
-        rebounds: 3,
-        assists: 2,
-        steals: 1,
-        blocks: 1,
-        slam_dunks: 0
-      }, {
-        player_name: "Brendan Haywood",
-        number: 33,
-        shoe: 15,
-        points: 6,
-        rebounds: 12,
-        assists: 12,
-        steals: 22,
-        blocks: 5,
-        slam_dunks: 12
-      }
-    ]
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        }, {
+          player_name: "Bismak Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        }, {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        }, {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        }, {
+          player_name: "Brendan Haywood",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
   }
-}
-
-# The values of the :home and :away keys are hashes
-
-# game_stat
 end
