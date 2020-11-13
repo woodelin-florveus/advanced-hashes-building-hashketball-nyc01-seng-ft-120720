40a4df2d1df0dec8da4670eb914afa0beb9e9867
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:24:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,7 +81,7 @@ def game_hash
         :slam_dunks => 2
         },
         {
-        :player_name => "Bismak Biyombo",
+        :player_name => "Bismack Biyombo",
         :number => 0,
         :shoe => 16,
         :points => 12,
