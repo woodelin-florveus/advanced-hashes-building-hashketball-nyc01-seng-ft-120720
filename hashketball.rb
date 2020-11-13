9324e667a75e2720593512eb110bd9baf0bf07de
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:13:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -106,7 +106,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 0
         }, {
-          player_name: "Kemba Walker",
+          player_name: "Brendan Haywood",
           number: 33,
           shoe: 15,
           points: 6,
