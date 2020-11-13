0d3cf8c329b2a6ad41cb5c0201eedf5064612d2b
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 20:27:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,11 +6,11 @@ def game_hash(data)
 
 
   binding.pry
-# game_stat = Hash.new
-#
-# # The values of the :home and :away keys are hashes
-#
-# game_stat[:home]
-#
-# game_stat
+game_stat = Hash.new
+
+# The values of the :home and :away keys are hashes
+
+game_stat[:home]
+
+game_stat
 end
