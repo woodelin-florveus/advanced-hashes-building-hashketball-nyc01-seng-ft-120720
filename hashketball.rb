41077920cda80856735f4cbe076f87e70d36250b
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:18:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,7 +70,7 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
-        :name => "Jeff Adrien",
+        :player_name => "Jeff Adrien",
         :number => 4,
         :shoe => 18,
         :points => 10,
