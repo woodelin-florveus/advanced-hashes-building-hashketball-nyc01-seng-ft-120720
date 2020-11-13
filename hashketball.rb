e84216bfbbb47bf493a851737955662d30ee8028
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:18:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,7 +20,7 @@ def game_hash
         :slam_dunks => 1
         },
         {
-        :name => "Reggie Evans",
+        :player_name => "Reggie Evans",
         :number => 30,
         :shoe => 14,
         :points => 12,
