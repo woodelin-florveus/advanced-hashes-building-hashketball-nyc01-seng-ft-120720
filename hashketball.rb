acbf476b045f7d9f725f043379251a2652f0341c
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:22:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ def game_hash
         :points => 6,
         :rebounds => 12,
         :assists => 12,
-        :steals => 22,
+        :steals => 7,
         :blocks => 5,
         :slam_dunks => 12
         }
