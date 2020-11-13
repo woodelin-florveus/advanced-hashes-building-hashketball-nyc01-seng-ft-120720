21222b978602478542c739c59c3fa74b732381da
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-13 21:15:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,7 +66,7 @@ def game_hash
       ]
     },
     :away_team => {
-      :name => "Charlotte Hornets",
+      :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
